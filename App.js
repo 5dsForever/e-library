696045f71a373d6228c658e0fb4601e6e28652f4
5dsForever <110550565+5dsForever@users.noreply.github.com>
2023-03-29 17:22:47 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,5 @@
+import React,{Component} from "react";
+import BottomTabNavigator from "./components/NamedBottomTabNavigator";
+ export default class App extends Component {
+   render() {
+     return <BottomTabNavigator />; } }
